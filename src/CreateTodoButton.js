d1d6c1ae5f352643831c0122758e3039f8af798b
= <=>
2021-12-16 22:39:49 -0600

    Agregando eventos

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,18 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
+
+    const onClickCreateButton = () => {
+      alert("I have new todo");
+    }
+
     return (
-      <button className="CreateTodoButton">+</button>
+      <button 
+        className="CreateTodoButton"
+        onClick={onClickCreateButton}
+      >
+        +
+      </button>
     );
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,30 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
+
+   const onClickCheck = (msg) => {
+      alert("Check todo "+ msg)
+   }
+
+   const onClickDelete = (msg) => {
+      alert("Delete todo "+msg)
+   }
+
     return (
       <li className="TodoItem">
-        <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+        <span 
+          className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+          onClick = {() => onClickCheck(props.text)}
+        >
           √
         </span>
         <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
           {props.text}
         </p>
-        <span className="Icon Icon-delete">
+        <span 
+          className="Icon Icon-delete"
+          onClick = {() => onClickDelete(props.text)}
+        >
           X
         </span>
       </li>
